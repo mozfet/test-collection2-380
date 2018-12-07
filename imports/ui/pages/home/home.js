294351d46e751bcece534ec264e11d59ff3e74b6
Mozfet <wynand@expertbox.com>
2018-12-07 16:03:21 +0200

    removed collection2

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,1 +1,40 @@
+// imports
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
+import SimpleSchema from 'simpl-schema'
 import './home.html'
+
+// define schema
+const schema = new SimpleSchema({
+  myField: String
+}, {tracker: Tracker})
+
+// on created
+Template.App_home.onCreated(() => {
+  const instance = Template.instance()
+})
+
+// on rendered
+Template.App_home.onRendered(() => {
+  const instance = Template.instance()
+})
+
+// helpers
+Template.App_home.helpers({
+  schema() {
+    return schema
+  }
+})
+
+// events
+Template.App_home.events({
+
+  //on click class
+  'click .className'(event, instance) {
+  }
+})
+
+// on destroyed
+Template.App_home.onDestroyed(() => {
+  const instance = Template.instance()
+})
